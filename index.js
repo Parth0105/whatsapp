103454f1c72c96b11de761386f26c2f206f98066
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2022-10-04 18:07:32 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.listen(process.env.PORT,()=>{
     console.log('Listening');
 });
 app.get('/webhooks',(req,res)=>{
-    const mode = req.query[hub.mode];
-    const challenge = req.query[hub.challenge];
-    const verify_token = req.query[hub.verify_token];
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['hub.challenge'];
+    const verify_token = req.query['hub.verify_token'];
     if(secret != verify_token ||  mode!= 'subscribe'){
         res.status(200).send(challenge);
     }
