1049b5d64d17f8e3e0d1f6ff5508399882cde4cb
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2022-10-06 22:31:02 +0530

    feat: axios support added

--- dist/config.js
+++ dist/config.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.secret = void 0;
-exports.secret = 'ghRt%6&7908GRECXdfghQ@213ioJL';
+exports.secret = process.env.verify_token;
