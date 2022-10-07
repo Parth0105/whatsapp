f6a90a7b63f90c7f14c85967acc7c6d5f6b7e2ec
parth0105pluang <98079965+parth0105pluang@users.noreply.github.com>
2022-10-07 08:26:54 +0530

    feat: axios support added

---
+++ src/index.ts
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyParsesr = require('body-parser');
+const app = express().use(bodyParsesr.json());
+import { secret } from './config';
+let axios;
+type mediaType = {
+    "object": string,
+        "entry": {
+                "id": string,
+                "changes":{
+                        "value": {
+                            "messaging_product": string,
+                            "metadata": {
+                                "display_phone_number": string,
+                                "phone_number_id": string
+                            },
+                            "contacts":
+                                {
+                                    "profile": {
+                                        "name": string
+                                    },
+                                    "wa_id": string
+                                }[],
+                            "messages":{
+                                    "from": string,
+                                    "id": string,
+                                    "timestamp": string,
+                                    "type": string,
+                                    "image": {
+                                        "mime_type": string,
+                                        "sha256": string,
+                                        "id": string
+                                    }
+                                }[]
+                        },
+                        "field": string
+                    }[]
+            }[]
+}
+type docType = {
+    "entry":{
+        "changes":{
+            "field": string,
+            "value": {
+                "contacts":{
+                        "profile": {
+                            "name": string
+                        },
+                        "wa_id": string
+                    }[],
+                "messages":{
+                        "document": {
+                            "filename": string,
+                            "id": string,
+                            "mime_type": string,
+                            "sha256": string
+                        },
+                        "from": string,
+                        "id": string,
+                        "timestamp": string,
+                        "type": string
+                    }[],
+                "messaging_product": string,
+                "metadata": {
+                    "display_phone_number": string,
+                    "phone_number_id": string
+                }
+            }
+        }[],
+            "id": string
+        }[]
+    "object": string
+}
+
+app.listen(process.env.PORT,async ()=>{
+    axios = await import('axios');
+    console.log('Listening');
+});
+app.get('/webhooks',(req,res)=>{
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['hub.challenge'];
+    const verify_token = req.query['hub.verify_token'];
+    if(secret != verify_token ||  mode!= 'subscribe'){
+        res.status(403);
+    }
+    res.status(200).send(challenge);
+});
+app.post('/webhooks', async (req, res) => {
+    const val = req.body;
+    console.log(JSON.stringify(val));
+    const imageId = val.entry[0].changes[0].value.messages[0].image.id || val.entry[0].changes[0].value.messages[0].document.id;
+    const baseURL = `https://graph.facebook.com/v15.0/${imageId}`;
+    const headers = {
+      Authorization:'Bearer EAASSYxSdSZCEBAKJydLVkf9KjeZAM6ylxbOg2NnhDkZCRkDc4i8G3Xx23Cw9goIPcxuFIE0UR99w7MoLayAhiOWsLD24H4KC4iduSK0M7bjVxecolFG7uxgzUB3fl3617Mqrz6uMdHwg9pXiZBZCpSQZAb0Js4I2MYiaqOnPLGHNwj6as8CzqEpeEnMvIWs8HUR6KvvZBNVwQZDZD',
+    };
+    let response;
+    try {
+        response = await axios.get(baseURL, { headers });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    res.status(200).send(response.data);
+});
+app.get('/', (req, res) => {
+  res.status(200).send('Server running');
+});
